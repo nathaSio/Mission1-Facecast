ae25934cfe95b50142884252d1413cfc0223d1b1
Salomouette <Salomethomas@free.fr>
2017-10-13 14:42:31 +0200

    Test

---
+++ Mission/app.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var stylus = require('stylus');
+
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var restapi = require('./rest/rest.js');
+var songControl = require('./routes/songs.js');
+
+
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var monk = require('monk');
+var db = monk('localhost:27017/musicjs');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+/* initialisation session et Passport */
+app.use(require('express-session')({
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(stylus.middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//base de donnée
+app.use(function(req, res, next){
+  req.db = db;
+  next();
+})
+
+// Utilisation des routes
+app.use('/', index);
+app.use('/users', users);
+app.use('/rest', restapi);
+app.use('/songs', songControl);
+
+// Config Passport
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// Config mongoose
+mongoose.connect('mongodb://localhost/musicjs');
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
